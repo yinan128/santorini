464eee635cc61fde9a262d047ba31b4f41881afa
yinan <yinanwu@andrew.cmu.edu>
2021-10-24 10:26:21 -0400

    Renamed logicManager to LogicController.

---
+++ src/main/java/edu/cmu/cs214/hw3/listeners/LogicController.java
@@ -0,0 +1,38 @@
+package edu.cmu.cs214.hw3.listeners;
+
+import edu.cmu.cs214.hw3.gameLogic.GameLogic;
+import edu.cmu.cs214.hw3.player.Player;
+import edu.cmu.cs214.hw3.player.Worker;
+
+import java.util.Map;
+
+public class LogicController implements EventListener {
+
+    Map<Player, GameLogic> logics;
+
+    public LogicController(Map<Player, GameLogic> logics) {
+        this.logics = logics;
+    }
+
+    @Override
+    public void castImpactAction(GameLogic gameLogic) {
+        gameLogic.castImpact(logics);
+    }
+
+    @Override
+    public void onMoveAction() {
+    }
+
+    @Override
+    public void onBuildAction() {
+    }
+
+    @Override
+    public void onSkipAction() {
+    }
+
+    @Override
+    public String toString() {
+        return "LogicManager";
+    }
+}
