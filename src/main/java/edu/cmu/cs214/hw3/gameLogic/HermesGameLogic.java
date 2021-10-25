01af1a50c9e30bc332bf52424ac625f6244cad18
yinan <yinanwu@andrew.cmu.edu>
2021-10-24 21:27:24 -0400

    created Hermes game logic.

---
+++ src/main/java/edu/cmu/cs214/hw3/gameLogic/HermesGameLogic.java
@@ -0,0 +1,32 @@
+package edu.cmu.cs214.hw3.gameLogic;
+
+import edu.cmu.cs214.hw3.core.Board;
+import edu.cmu.cs214.hw3.position.Location;
+
+/**
+ * Game logic of Hermes.
+ * If your Workers do not move up or down, they may each move any number of
+ * times (even zero), and then either builds.
+ * Skip allowed.
+ */
+public class HermesGameLogic extends GameLogicDecorator {
+
+
+    public HermesGameLogic(GameLogic gameLogic) {
+        super(gameLogic);
+    }
+
+
+    @Override
+    public boolean move(Board board, Location start, Location destination) {
+        if (board.deltaHeight(destination, start) == 0) {
+            return super.forceMove(board, start, destination);
+        }
+        return super.move(board, start, destination);
+    }
+
+    @Override
+    public void skip() {
+        informNextAction();
+    }
+}
