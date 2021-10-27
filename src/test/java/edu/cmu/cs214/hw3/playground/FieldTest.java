753f7d73512279400bfff664757f4ac52680d73b
yinan <yinanwu@andrew.cmu.edu>
2021-10-26 21:34:30 -0400

    unit test for Field.

--- src/test/java/edu/cmu/cs214/hw3/playground/FieldTest.java
+++ src/test/java/edu/cmu/cs214/hw3/playground/FieldTest.java
@@ -1,5 +1,8 @@
 package edu.cmu.cs214.hw3.playground;
 
+import edu.cmu.cs214.hw3.player.Player;
+import edu.cmu.cs214.hw3.player.Worker;
+import edu.cmu.cs214.hw3.util.Location;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,90 +12,54 @@ public class FieldTest {
 
     private Field field;
 
-//    @Before
-//    public void setup() {
-//        field = new Field();
-//    }
-//
-//    @Test
-//    public void testMoveStartOneLvlLower() {
-//        Field dest = new Field();
-//        dest.build();
-//        assertTrue(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testMoveStartTwoLvlLower() {
-//        Field dest = new Field();
-//        dest.build();
-//        dest.build();
-//        assertFalse(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testMoveDestOccupied() {
-//        Field dest = new Field();
-//        dest.occupy();
-//        assertFalse(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testMoveStartOneLvlHigher() {
-//        Field dest = new Field();
-//        field.build();
-//        assertTrue(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testMoveStartTwoLvlHigher() {
-//        Field dest = new Field();
-//        field.build();
-//        field.build();
-//        assertTrue(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testMoveStartTwoLvlHigherDestOccupied() {
-//        Field dest = new Field();
-//        dest.occupy();
-//        field.build();
-//        field.build();
-//        assertFalse(Field.isMovable(field, dest));
-//    }
-//
-//    @Test
-//    public void testBuildOneFloor() {
-//        assertTrue(field.build());
-//    }
-//
-//    @Test
-//    public void testBuildTwoFloors() {
-//        for (int i = 0; i < 2; i++) {
-//            assertTrue(field.build());
-//        }
-//    }
-//
-//    @Test
-//    public void testBuildThreeFloors() {
-//        for (int i = 0; i < 3; i++) {
-//            assertTrue(field.build());
-//        }
-//    }
-//
-//    @Test
-//    public void testBuildFiveFloors() {
-//        for (int i = 0; i < 4; i++) {
-//            assertTrue(field.build());
-//        }
-//        assertFalse(field.build());
-//    }
-//
-//    @Test
-//    public void testBuildWhenOccupied() {
-//        field.build();
-//        field.occupy();
-//        assertFalse(field.build());
-//    }
+    @Before
+    public void setup() {
+        field = new Field(Location.get(0, 0));
+    }
+
+    @Test
+    public void testMoveStartOneLvlLower() {
+        Field dest = new Field(Location.get(1, 1));
+        dest.build();
+        assertTrue(Field.isMovable(field, dest));
+    }
+
+    @Test
+    public void testMoveStartTwoLvlLower() {
+        Field dest = new Field(Location.get(1, 1));
+        dest.build();
+        dest.build();
+        assertFalse(Field.isMovable(field, dest));
+    }
+
+    @Test
+    public void testMoveStartOneLvlHigher() {
+        Field dest = new Field(Location.get(1, 1));
+        field.build();
+        assertTrue(Field.isMovable(field, dest));
+    }
+
+    @Test
+    public void testMoveStartTwoLvlHigher() {
+        Field dest = new Field(Location.get(1, 1));
+        field.build();
+        field.build();
+        assertTrue(Field.isMovable(field, dest));
+    }
+
+
+    @Test
+    public void testAddworker() {
+        field.setWorker(new Worker(Location.get(1, 1), new Player(0)));
+        assertTrue(field.isOccupied());
+    }
+
+    @Test
+    public void testRemoveWorker() {
+        field.setWorker(new Worker(Location.get(1, 1), new Player(0)));
+        field.removeWorker();
+        assertFalse(field.isOccupied());
+    }
 
 
 }
