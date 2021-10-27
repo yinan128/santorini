ec8a1b67ff724b0bf820d764c0eb1c823f3e99db
yinan <wooyehnan@gmail.com>
2021-10-27 19:43:36 -0400

    integration tests for Game class.

--- src/test/java/edu/cmu/cs214/hw3/core/GameTest.java
+++ src/test/java/edu/cmu/cs214/hw3/core/GameTest.java
@@ -3,13 +3,224 @@ package edu.cmu.cs214.hw3.core;
 import edu.cmu.cs214.hw3.gameLogic.*;
 import edu.cmu.cs214.hw3.player.Player;
 import edu.cmu.cs214.hw3.util.Location;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class GameTest {
-    public static void main(String[] args) {
+
+    private Game game;
+    private Player playerA;
+    private Player playerB;
+
+
+    @Before
+    public void setup() {
+        game = new Game();
+        playerA = game.getPlayers().get(0);
+        playerB = game.getPlayers().get(1);
+
+        // game test without god card.
+        game.placeWorker(playerA, Location.get(1, 1));
+        game.placeWorker(playerA, Location.get(3, 3));
+        game.placeWorker(playerB, Location.get(1, 3));
+        game.placeWorker(playerB, Location.get(3, 1));
+        /* initial state of board.
+         * - - - - -
+         * - A - B -
+         * - - - - -
+         * - B - A -
+         * - - - - -
+         */
+    }
+
+    // ------------------ game without god ------------------
+    @Test
+    public void integrationTest_withoutGod_testSuccess() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 2));
+
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(1, 1));
+        game.build(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.moveWorker(playerB, Location.get(2, 3), Location.get(1, 3));
+        game.build(playerB, Location.get(1, 3), Location.get(2, 2));
+
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(1, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(1, 2));
+
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(1, 2));
+        game.build(playerA, Location.get(1, 2), Location.get(1, 1));
+        game.moveWorker(playerB, Location.get(3, 1), Location.get(4, 0));
+        game.build(playerB, Location.get(4, 0), Location.get(4, 1));
+
+        assertNull(game.getWinner());
+        game.moveWorker(playerA, Location.get(1, 2), Location.get(2, 2));
+        assertEquals(playerA, game.getWinner());
+    }
+
+    @Test
+    public void integrationTest_withoutGod_testInValidMove_workerNotExist() {
+        assertFalse(game.moveWorker(playerA, Location.get(0, 0), Location.get(1, 1)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_testInValidMove_moveTooFar() {
+        assertFalse(game.moveWorker(playerA, Location.get(1, 1), Location.get(4, 4)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_testInValidMove_Occupied() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        assertFalse(game.moveWorker(playerB, Location.get(3, 1), Location.get(2, 1)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_testInValidMove_TwoLevelHigh() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 2));
+        assertFalse(game.moveWorker(playerA, Location.get(2, 1), Location.get(2, 2)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_testInValidMove_Domed() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 2));
+
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(1, 2));
+        game.build(playerA, Location.get(1, 2), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(2, 3), Location.get(1, 3));
+        game.build(playerB, Location.get(1, 3), Location.get(2, 2));
+
+        assertFalse(game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 2)));
+    }
+
+
+
+    @Test
+    public void integrationTest_withoutGod_wrongSequence_doubleMove() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        assertFalse(game.moveWorker(playerA, Location.get(2, 1), Location.get(2, 2)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_wrongSequence_wrongPlayer() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        assertFalse(game.moveWorker(playerB, Location.get(1, 3), Location.get(0, 3)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_wrongSequence_doubleBuild() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        assertFalse(game.build(playerA, Location.get(2, 1), Location.get(2, 2)));
+    }
+
+    @Test
+    public void integrationTest_withoutGod_wrongSequence_manipulateDifferentWorkers() {
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        assertFalse(game.build(playerA, Location.get(3, 3), Location.get(4, 4)));
+    }
+
+    // ------------------ game with god ------------------
+
+    // ------------------ game with athena ------------------
+    @Test
+    public void test_Athena_castPower() {
+        // assign Athena to playerA.
+        game.assignGameLogic(playerA, new AthenaGameLogic(new BasicGameLogic()));
+
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 4));
+
+        // athena move up.
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.build(playerA, Location.get(2, 2), Location.get(2, 1));
+        // opponent is blocked to move up.
+        game.printBoard();
+        assertFalse(game.moveWorker(playerB, Location.get(2, 3), Location.get(2, 4)));
+    }
+
+    @Test
+    public void test_Athena_powerOffAfterOneFlatMove() {
+        // assign Athena to playerA.
+        game.assignGameLogic(playerA, new AthenaGameLogic(new BasicGameLogic()));
+
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 4));
+
+        // athena move up.
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.build(playerA, Location.get(2, 2), Location.get(2, 1));
+        // flat move by opponent worker
+        game.moveWorker(playerB, Location.get(2, 3), Location.get(1, 3));
+        game.build(playerB, Location.get(1, 3), Location.get(1, 4));
+
+        // athena move down.
+        game.moveWorker(playerA, Location.get(2, 2), Location.get(1, 2));
+        game.build(playerA, Location.get(1, 2), Location.get(2, 2));
+        // opponent worker can move up now.
+        assertTrue(game.moveWorker(playerB, Location.get(1, 3), Location.get(1, 4)));
+    }
+
+    // ------------------ game with pan ------------------
+    @Test
+    public void test_Pan_winningCondition() {
+        // assign Athena to playerA.
+        game.assignGameLogic(playerB, new PanGameLogic(new BasicGameLogic()));
+
+        game.moveWorker(playerA, Location.get(1, 1), Location.get(2, 1));
+        game.build(playerA, Location.get(2, 1), Location.get(2, 2));
+        game.moveWorker(playerB, Location.get(1, 3), Location.get(2, 3));
+        game.build(playerB, Location.get(2, 3), Location.get(2, 2));
+
+        game.moveWorker(playerA, Location.get(2, 1), Location.get(1, 1));
+        game.build(playerA, Location.get(1, 1), Location.get(1, 2));
+        game.moveWorker(playerB, Location.get(2, 3), Location.get(1, 2));
+        game.build(playerB, Location.get(1, 2), Location.get(0, 1));
+
+        game.moveWorker(playerA, Location.get(3, 3), Location.get(3, 4));
+        game.build(playerA, Location.get(3, 4), Location.get(4, 4));
+        game.moveWorker(playerB, Location.get(1, 2), Location.get(2, 2));
+        game.build(playerB, Location.get(2, 2), Location.get(1, 2));
+
+        game.moveWorker(playerA, Location.get(3, 4), Location.get(3, 3));
+        game.build(playerA, Location.get(3, 3), Location.get(4, 4));
+        assertNull(game.getWinner());
+        // pan move down by 2 levels.
+        game.moveWorker(playerB, Location.get(2, 2), Location.get(2, 3));
+        assertEquals(playerB, game.getWinner());
+    }
+
+
+
+    public static void notmain(String[] args) {
 
         Game game = new Game();
         Player player1 = game.getPlayers().get(0);
         Player player2 = game.getPlayers().get(1);
+
+        // game test without god card.
+        game.placeWorker(player1, Location.get(0, 0));
+        game.placeWorker(player1, Location.get(4, 4));
+        game.placeWorker(player2, Location.get(4, 0));
+        game.placeWorker(player2, Location.get(0, 4));
+
+
+
 
 //        // Minotaur test
 //        game.assignGameLogic(player2, new MinotaurGameLogic(new BasicGameLogic()));
