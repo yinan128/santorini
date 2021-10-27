a7ea09e28489e9c3152018705b6388160bbcdf44
yinan <yinanwu@andrew.cmu.edu>
2021-10-26 21:45:00 -0400

    unit test for Worker

--- src/test/java/edu/cmu/cs214/hw3/player/WorkerTest.java
+++ src/test/java/edu/cmu/cs214/hw3/player/WorkerTest.java
@@ -1,45 +1,48 @@
 package edu.cmu.cs214.hw3.player;
 
+import edu.cmu.cs214.hw3.util.Location;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class WorkerTest {
 
     private Worker worker;
+    private Player player;
 
-//    @Before
-//    public void setup() {
-//        worker = new Worker(new Location(5, 5));
-//    }
-//
-//    @Test
-//    public void testMoveUp() {
-//        worker.moveAtDir(Direction.UP);
-//        assertEquals(new Location(4, 5), worker.getLocation());
-//    }
-//
-//    @Test
-//    public void testMoveUpLeft() {
-//        worker.moveAtDir(Direction.UPLEFT);
-//        assertEquals(new Location(4, 4), worker.getLocation());
-//    }
-//
-//    @Test
-//    public void testMoveDownRight() {
-//        worker.moveAtDir(Direction.DOWNRIGHT);
-//        assertEquals(new Location(6, 6), worker.getLocation());
-//    }
-//
-//    @Test
-//    public void testDestinationUp() {
-//        assertEquals(new Location(4, 5), worker.getDestination(Direction.UP));
-//        // shouldn't change the worker's location
-//        assertEquals(new Location(5, 5), worker.getLocation());
-//    }
-//
-//    @Test
-//    public void testDestinationDownLeft() {
-//        assertEquals(new Location(6, 4), worker.getDestination(Direction.DOWNLEFT));
-//        // shouldn't change the worker's location
-//        assertEquals(new Location(5, 5), worker.getLocation());
-//    }
+    @Before
+    public void setup() {
+        player = new Player(0);
+        worker = new Worker(Location.get(0, 0), player);
+    }
+
+    @Test
+    public void testSamePlayer() {
+        Worker anotherWorker = new Worker(Location.get(1, 1), player);
+        assertTrue(Worker.fromSamePlayer(worker, anotherWorker));
+    }
+
+    @Test
+    public void testDifferentPlayer() {
+        Worker anotherWorker = new Worker(Location.get(1, 1), new Player(1));
+        assertFalse(Worker.fromSamePlayer(worker, anotherWorker));
+    }
+
+    @Test
+    public void testNextState() {
+        Worker nextState = worker.moveTo(Location.get(1, 1));
+        assertEquals(Location.get(1, 1), nextState.getLocation());
+    }
+
+    @Test
+    public void testPrevState() {
+        Worker nextState = worker.moveTo(Location.get(1, 1));
+        assertEquals(Location.get(0, 0), nextState.getPrevState().getLocation());
+    }
+
+
+
 
 
 }
